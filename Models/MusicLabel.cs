080fe4d7fb802543f5e9b059e8a325f87eaf9260
ProudTurtle <s22477@pjwstk.edu.pl>
2022-06-09 14:25:06 +0200

    dodano modele

---
+++ Models/MusicLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolokwium2.Models
+{
+    public class MusicLabel
+    {
+        public int IdMusicLabel { get; set; }
+        public String Name { get; set; }
+
+        public virtual IEnumerable<Album> Albums { get; set; }
+
+    }
+}
---
+++ Models/Musician_Track.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolokwium2.Models
+{
+    public class Musician_Track
+    {
+        public virtual Musician Musician { get; set; }
+        public virtual Track Track { get; set; }
+
+    }
+}
